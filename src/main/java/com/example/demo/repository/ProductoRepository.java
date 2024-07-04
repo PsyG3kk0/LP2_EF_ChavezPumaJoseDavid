5a9bdb81f0571363601685a30702098745733cc6
David Chavez <jose_17298@hotmail.com>
2024-07-04 01:22:55 -0500

    Terminando los servicesImpl

--- src/main/java/com/example/demo/repository/ProductoRepository.java
+++ src/main/java/com/example/demo/repository/ProductoRepository.java
@@ -1,5 +1,11 @@
 package com.example.demo.repository;
 
-public interface ProductoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.ProductoEntity;
+
+@Repository
+public interface ProductoRepository  extends JpaRepository<ProductoEntity, Integer>{
 
 }
